bb742e25e2cff1b996c2cb7b23447f1c428a3337
RobertMartinez <robert.david.martinez@gmail.com>
2023-07-17 09:11:06 +0700

    corrected bootstrap link and add case insensitivity to password

--- script.js
+++ script.js
@@ -1,6 +1,14 @@
+function checkKeyPress(event) {
+  if (event.keyCode === 13) { // 13 is the key code for "Enter" key
+    checkAccess();
+  }
+}
+
 function checkAccess() {
-  var accessCode = document.getElementById('accessCode').value;
-  if (accessCode === 'password') {
+  var accessCode = document.getElementById('accessCode').value.toLowerCase(); // Convert to lowercase
+  var expectedCode = 'password'; // Expected password, you can change it as needed
+
+  if (accessCode === expectedCode.toLowerCase()) { // Convert to lowercase for comparison
     window.location.href = 'second-page.html';
   } else {
     alert('Incorrect access code. Please try again.');
